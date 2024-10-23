632d385fcc64ec3fd8e7e2a990bdbfef95af182f
dstyq <distidisti23@gmail.com>
2024-10-23 12:06:22 +0700

    update

--- resources/views/gardu/index.blade.php
+++ resources/views/gardu/index.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+
     <h1>Data Gardu</h1>
-    <a href="{{ route('gardu.create') }}" class="btn btn-primary">Tambah Gardu</a>
-    <table class="table">
+    <a href="{{ route('gardu.create') }}" class="btn btn-primary mb-3">Tambah Gardu</a>
+    <table class="table table-striped">
         <thead>
             <tr>
                 <th>ID</th>
@@ -12,20 +13,27 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($gardus as $gardu)
+            @if ($gardus->isEmpty())
                 <tr>
-                    <td>{{ $gardu->id }}</td>
-                    <td>{{ $gardu->nama }}</td>
-                    <td>
-                        <a href="{{ route('gardu.edit', $gardu->id) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('gardu.destroy', $gardu->id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Hapus</button>
-                        </form>
-                    </td>
+                    <td colspan="3" class="text-center">Tidak ada data gardu.</td>
                 </tr>
-            @endforeach
+            @else
+                @foreach ($gardus as $gardu)
+                    <tr>
+                        <td>{{ $gardu->id }}</td>
+                        <td>{{ $gardu->nama }}</td>
+                        <td>
+                            <a href="{{ route('gardu.edit', $gardu->id) }}" class="btn btn-warning">Edit</a>
+                            <form action="{{ route('gardu.destroy', $gardu->id) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Hapus</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            @endif
         </tbody>
     </table>
+    
 @endsection
