9c074db5e42c5782ad502bf98e893d61837a6f67
dstyq <distidisti23@gmail.com>
2024-10-31 11:35:46 +0700

    update

--- resources/views/gardu/index.blade.php
+++ resources/views/gardu/index.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
+@section('breadcumb')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <h4 class="mb-sm-0 font-size-18">{{ $breadcumb ?? 'Gardu' }}</h4>
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                        <li class="breadcrumb-item active">{{ $breadcumb ?? 'Gardu' }}</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
 @section('content')
 <div class="container">
     <h1>Data Gardu</h1>
